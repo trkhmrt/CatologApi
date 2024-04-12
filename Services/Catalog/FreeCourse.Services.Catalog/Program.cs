000ea49c7da1024790a6ca2555a89b01819c5357
trkhmrt <trkhamarat@gmail.com>
2024-04-13 01:42:20 +0300

    .

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -2,6 +2,8 @@
 using FreeCourse.Services.Catalog.Mapping;
 using FreeCourse.Services.Catalog.Services;
 using FreeCourse.Services.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
 
 namespace FreeCourse.Services.Catalog;
@@ -14,13 +16,26 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddAutoMapper(typeof(GeneralMapping));
-        builder.Services.AddControllers();
+        builder.Services.AddControllers(opt =>
+        {
+            opt.Filters.Add(new AuthorizeFilter());//Bu şekilde olduğunda bütün projedeki controllerlar authorize filtresine sahip oldu.
+        });
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         builder.Services.AddScoped<ICategoryService, CategoryService>();
         builder.Services.AddScoped<ICourseService, CourseService>();
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = builder.Configuration["IdentityServerUrl"];//appsetting.jsondan alıyor
+                options.Audience = "resource_catalog"; //bu isim IdentityServer configde ApiResource'den geliyor.
+                options.RequireHttpsMetadata = false;
 
+
+            });
+
+            
 
         var test = builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
 
@@ -39,6 +54,8 @@ public class Program
             app.UseSwaggerUI();
         }
 
+        //app.UseRouting(); buna sanırım gerek yok.Defaultda yoktu ben ekledim.
+        app.UseAuthentication();
         app.UseAuthorization();
 
 
